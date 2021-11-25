f6d20490bdbf430fc153dd92f539d6dd69fd54dd
Shubham-Kumar-2000 <shukhu10@gmail.com>
2021-11-25 22:09:01 +0530

    Cors in workers added

--- gateway/adminWorker.js
+++ gateway/adminWorker.js
@@ -1,3 +1,8 @@
+const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+    'Access-Control-Max-Age': '86400'
+};
 addEventListener('fetch', async (event) => {
     event.respondWith(
         handleRequest(event.request).catch(
@@ -5,18 +10,48 @@ addEventListener('fetch', async (event) => {
         )
     );
 });
+function handleOptions(request) {
+    // Make sure the necessary headers are present
+    // for this to be a valid pre-flight request
+    const headers = request.headers;
+    if (
+        headers.get('Origin') !== null &&
+        headers.get('Access-Control-Request-Method') !== null &&
+        headers.get('Access-Control-Request-Headers') !== null
+    ) {
+        const respHeaders = {
+            ...corsHeaders,
+            'Access-Control-Allow-Headers': request.headers.get(
+                'Access-Control-Request-Headers'
+            )
+        };
+
+        return new Response(null, {
+            headers: respHeaders
+        });
+    } else {
+        return new Response(null, {
+            headers: {
+                Allow: 'GET, HEAD, POST, OPTIONS'
+            }
+        });
+    }
+}
 const handleRequest = async (request) => {
+    if (request.method === 'OPTIONS') {
+        return handleOptions(request);
+    }
     if (request.method == 'POST') {
         const data = await request.json();
         await CONFIG.put(`data`, JSON.stringify(data));
-        return new Response('OK', { status: 200 });
+        return new Response('OK', { status: 200, headers: corsHeaders });
     }
     if (request.method == 'GET') {
         const data = await request.json();
         await CONFIG.get(`data`);
         return new Response(JSON.stringify(data), {
             status: 200,
-            headers: { 'Content-Type': 'application/json' }
+            headers: { ...corsHeaders, 'Content-Type': 'application/json' }
         });
     }
     return new Response('Not Found', { status: 404 });
